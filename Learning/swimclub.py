6027fb20efe0492111026fd40c144863d92151cc
japsimar-soin <japsimarsoin2003@gmail.com>
2026-01-18 23:26:30 +0530

    Charts

---
+++ Learning/swimclub.py
@@ -0,0 +1,31 @@
+import statistics
+
+FOLDER = "swimdata/"
+
+def read_swim_data(filename):
+  swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
+
+  with open(FOLDER + filename) as file:
+    lines = file.readlines()
+    times = lines[0].strip().split(",")
+
+  converts = []
+  for t in times:
+    if ":" in t:
+      minutes, rest = t.split(":")
+      seconds, hundredths = rest.split(".")
+    else:
+      minutes = 0
+      seconds, hundredths = t.split(".")
+
+    converted_time = (int(minutes)*60*100)+(int(seconds)*100)+(int(hundredths))
+    converts.append(converted_time)
+
+  average = statistics.mean(converts)
+  mins_secs, hundredths = str(round(average/100, 2)).split(".")
+  mins_secs = int(mins_secs)
+  minutes = mins_secs//60
+  seconds = mins_secs-minutes*60
+  average = f"{minutes}:{seconds}.{hundredths}"
+
+  return swimmer, age, distance, stroke, times, average, converts
